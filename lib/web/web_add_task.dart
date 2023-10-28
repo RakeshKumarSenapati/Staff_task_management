06b7be38be0c2a8bccf2d847d01502c1fa39798c
RakeshKumarSenapati <rakeshsenapati1030@gmail.com>
2023-10-28 17:42:30 +0530

    add task ui

--- lib/web/web_add_task.dart
+++ lib/web/web_add_task.dart
@@ -70,8 +70,6 @@ class _Web_Add_TAskState extends State<Web_Add_TAsk> {
 
   @override
   Widget build(BuildContext context) {
-    var name = "Name";
-    var desg = "developer";
     return Scaffold(
         body: SingleChildScrollView(
       child: Container(
@@ -269,201 +267,17 @@ class _Web_Add_TAskState extends State<Web_Add_TAsk> {
                                   ),
                                 ),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 23),
-                                child: ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                    "Delete",
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.w900),
-                                  ),
-                                  style: ButtonStyle(
-                                      foregroundColor:
-                                          MaterialStateProperty.all(
-                                              Colors.white),
-                                      backgroundColor:
-                                          MaterialStateProperty.all(
-                                              Colors.blue),
-                                      elevation:
-                                          MaterialStateProperty.all<double>(
-                                              5.0),
-                                      shadowColor:
-                                          MaterialStateProperty.all<Color>(
-                                              Colors.black),
-                                      minimumSize:
-                                          MaterialStateProperty.all<Size>(
-                                              const Size(100.0, 50.0)),
-                                      shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(13),
-                                            side: const BorderSide(
-                                                color: Colors.black, width: 2)),
-                                      ),
-                                      overlayColor: MaterialStateProperty.all(
-                                          const Color.fromARGB(
-                                              255, 51, 24, 148))),
-                                ),
-                              )
+                              
                             ],
                           ),
                         ),
                       ]),
                     ),
                   ),
-                  SingleChildScrollView(
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 130, left: 150),
-                      width: 800,
-                      height: 500,
-                      // color: Colors.amber,
-                      child: Column(
-                        children: [
-                          Container(
-                            width: 800,
-                            height: 300,
-                            // color: Colors.amber,
-                            child: ListView.builder(
-                              itemBuilder: (context, index) {
-                                return const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Card(
-                                      elevation: 5,
-                                      child: Column(
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.all(8.0),
-                                            child: Text(
-                                              "Title",
-                                              style: TextStyle(
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.w600),
-                                            ),
-                                          ),
-                                          Text(
-                                            "Description",
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.w400),
-                                          )
-                                        ],
-                                      )),
-                                );
-                              },
-                              itemCount: 10,
-                            ),
-                          ),
-                          Container(
-                              padding: const EdgeInsets.only(left: 700),
-                              child: const InkWell(
-                                child: Text(
-                                  "more->",
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.blue),
-                                ),
-                              )),
-                        ],
-                      ),
-                    ),
-                  )
+                  
                 ],
               ),
             ),
-            /* Container(
-              margin: EdgeInsets.only(top: 23),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Task description",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
-                        child: Text(
-                      "more->",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.blue),
-                    )),
-                  )
-                ],
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                margin: EdgeInsets.only(top: 5),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 180,
-                        height: 150,
-                        child: Card(
-                            elevation: 11,
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Title ",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
-                                Text(
-                                  "Description",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w400),
-                                )
-                              ],
-                            )),
-                      ),
-                      Container(
-                        width: 180,
-                        height: 150,
-                        margin: EdgeInsets.only(left: 20),
-                        child: Card(
-                            elevation: 5,
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Title",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ),
-                                Text(
-                                  "Description",
-                                  style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w400),
-                                )
-                              ],
-                            )),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            )
-          */
           ],
         ),
       ),
