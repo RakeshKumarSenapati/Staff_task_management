6e13fabd3147f4fb5f9f9d8175bd1c71156679ab
priyadarsan1234 <priyadarshanpradhanrinku@gmail.com>
2024-02-16 21:11:08 +0530

    changes for pdf admin side design change give a professional look

--- lib/mobile/Report_retrive.dart
+++ lib/mobile/Report_retrive.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:animate_do/animate_do.dart';
 
 class Report_Retrive extends StatefulWidget {
-  // const Report_Retrive({Key? key}) : super(key: key);
   final String id;
   final String name;
 
@@ -18,30 +17,38 @@ class Report_Retrive extends StatefulWidget {
 
 class _RetriveState extends State<Report_Retrive> {
   List<dynamic> items = [];
+  List<String> months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  String selectedMonth = 'January'; // Default month
 
   Future<void> fetchData() async {
-    var url =
-        Uri.parse('https://creativecollege.in/Flutter/Retrive_Report.php');
+    var url = Uri.parse('https://creativecollege.in/Flutter/Retrive_Report.php');
 
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
       setState(() {
-        // items = json.decode(response.body);
-
         List<dynamic> allItems = json.decode(response.body);
-        items = allItems.where((allItems) => allItems['ID'] == widget.id).toList();
+        items = allItems.where((item) => item['ID'] == widget.id && item['Month'] == selectedMonth).toList();
 
-        if(items.isEmpty){
-          Fluttertoast.showToast(msg: 'Is empty file not available');
+        if (items.isEmpty) {
+          Fluttertoast.showToast(msg: 'No files available for $selectedMonth');
+        } else {
+          Fluttertoast.showToast(msg: 'Showing files for $selectedMonth');
         }
-        else{
-          
-          Fluttertoast.showToast(msg: widget.id);
-        }
-
       });
-    
     } else {
       print('Failed to load data');
     }
@@ -55,23 +62,68 @@ class _RetriveState extends State<Report_Retrive> {
 
   @override
   Widget build(BuildContext context) {
-    // String Name = widget.name.indexOf(' ') != 0 ? widget.name.substring(0,widget.name.indexOf(' '));
+    const _color1 = Color(0xFFC21E56);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.name+' Report',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+      appBar:AppBar(
+        backgroundColor: _color1,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+        
+        title: Text(
+          '${widget.name} Report',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return FadeInLeft(
-            // Wrap your widget with FadeInUp
-            duration: Duration(milliseconds: 300),
-            delay: Duration(milliseconds: index * 50),
-            child: StaffCard(item: items[index], fetchData: fetchData),
-          );
-        },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: DropdownButton<String>(
+              value: selectedMonth,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedMonth = newValue!;
+                });
+                fetchData(); // Refetch data based on the selected month
+              },
+              items: months.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                );
+              }).toList(),
+              style: TextStyle(color: Colors.black),
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 36.0,
+              elevation: 16,
+              isExpanded: true,
+              underline: Container(
+                height: 2,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return FadeInLeft(
+                  duration: Duration(milliseconds: 300),
+                  delay: Duration(milliseconds: index * 50),
+                  child: StaffCard(item: items[index]),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -79,27 +131,33 @@ class _RetriveState extends State<Report_Retrive> {
 
 class StaffCard extends StatelessWidget {
   final Map<String, dynamic> item;
-  final Function fetchData;
 
-  StaffCard({required this.item, required this.fetchData});
+  StaffCard({required this.item});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
       child: Padding(
         padding: EdgeInsets.all(8),
         child: ListTile(
-          title: Text(item['Filename']),
-          onTap: () {
+          title: Text(
+            item['Filename'],
+            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w700),
             
+          ),
+          subtitle: Text(item['Month']),
+          onTap: () {
             String name = item['Filename'];
             Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => PDFViewer(
-                      url:
-                          "https://creativecollege.in/Flutter/Report/Pdflist.php?name=$name")),
+                      url: "https://creativecollege.in/Flutter/Report/Pdflist.php?name=$name")),
             );
           },
         ),
